92404f5b404a03d2e439ca96395aee3d1347ea59
Nagesh Bingi <46963154+nagi1995@users.noreply.github.com>
2020-12-14 10:03:30 +0530

    Add files via upload

---
+++ MNIST_pca_svm.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 13 11:00:26 2020
+
+@author: Nagesh
+"""
+#%%
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score
+import pickle
+
+
+#%%
+
+
+df = pd.read_csv("mnist_data.csv", delimiter = ',')
+xy = np.array(df, dtype = 'float')
+x, y = xy[:, :-1], xy[:, -1]
+
+#%%
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.99, random_state = 4)
+
+#%%
+
+y_train_df = pd.Series(y_train)
+print(np.sort(pd.unique(y_train_df)))
+
+#%%
+
+pca = PCA()
+pca.fit(x_train)
+plt.plot(pca.explained_variance_ratio_.cumsum())
+
+#%%
+
+print(np.where(pca.explained_variance_ratio_.cumsum() > 0.9))
+print(np.where(pca.explained_variance_ratio_.cumsum() > 0.95))
+print(np.where(pca.explained_variance_ratio_.cumsum() > 0.99))
+
+#%%
+
+param_grid = {"n_components" : [74, 123, 255]}
+pca = PCA()
+gs = GridSearchCV(estimator = pca, param_grid = param_grid)
+gs.fit(x_train, y_train)
+
+print(gs.best_params_)
+
+#%%
+
+pca = PCA(n_components = 123).fit(x_train)
+
+#%%
+
+with open('pca_model.pkl', 'wb') as f:
+    pickle.dump(pca, f)
+
+#%%
+
+x_train_pca = pca.transform(x_train)
+
+#%%
+
+model = SVC(kernel = 'rbf').fit(x_train_pca, y_train)
+
+#%%
+
+
+with open('mnist_pca_svm.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+with open('mnist_pca_svm.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+#%%
+
+x_test_pca = pca.transform(x_test)
+y_pred = model.predict(x_test_pca)
+print(accuracy_score(y_test, y_pred))
+
+
+
+
+
+
+
---
+++ gui.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 13 17:40:31 2020
+
+@author: Nagesh
+"""
+
+#%%
+
+from tkinter import *
+import numpy as np
+import pandas as pd
+import pickle
+import cv2
+
+#%%
+
+df = pd.read_csv("mnist_data.csv", delimiter = ",")
+xy = np.array(df, dtype = 'float')
+
+#%%
+with open('pca_model.pkl', 'rb') as f:
+    pca = pickle.load(f)
+
+
+with open('mnist_pca_svm.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+
+#%%
+
+
+fontsize = 15
+window1 = Tk()
+window1.title("GUI")
+
+def mnist():
+    idx = np.random.randint(0, xy.shape[1])
+    x = xy[idx, :-1].reshape(1, -1)
+    y_pred = int(model.predict(pca.transform(x)))
+    y_true = int(xy[idx, -1])
+    print(idx)
+    shape = (480, 480)
+    x = x.reshape(28, 28)
+    im = cv2.resize(x, (360, 360), interpolation = cv2.INTER_AREA)
+    true_image = cv2.imread("images/" + str(y_true) + ".jpg")
+    true_image = cv2.resize(true_image, shape, interpolation = cv2.INTER_AREA)
+    pred_image = cv2.imread("images/" + str(y_pred) + ".jpg")
+    pred_image = cv2.resize(pred_image, shape, interpolation = cv2.INTER_AREA)
+    
+    cv2.imshow("mnist_image", im)
+    cv2.imshow("predicted_label", pred_image)
+    cv2.imshow("true_label", true_image)
+    
+button = Button(window1, text = "Click", command = mnist)
+button.pack()
+window1.mainloop()
+
